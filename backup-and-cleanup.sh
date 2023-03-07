13187ed364c18606a146ce2ae1ea8895ce0ca862
unknown <kevlar2020@gmail.com>
2023-03-07 14:40:44 -0600

    bugfix

--- backup-and-cleanup.sh
+++ backup-and-cleanup.sh
@@ -25,36 +25,40 @@ then
 fi
 
 #try to find the target backup name. gets the most recent modified .zip file in the specified backup directory
-echo "Trying to find most recent backup with file type .$FILE_EXTENSION in directory $BACKUP_SOURCE_DIR"
-TARGET=$(ls -Art ${BACKUP_SOURCE_DIR} | grep ".${FILE_EXTENSION}$" | tail -n $NUM_MOST_RECENT_FILES)
-if [[ -z ${TARGET} ]]; then
+echo "Trying to find $NUM_MOST_RECENT_FILES most recent backup(s) with file type .$FILE_EXTENSION in directory $BACKUP_SOURCE_DIR"
+TARGETS=$(ls -Art ${BACKUP_SOURCE_DIR} | grep ".${FILE_EXTENSION}$" | tail -n $NUM_MOST_RECENT_FILES)
+UPLOAD_STATES='```\n'
+if [[ -z ${TARGETS} ]]; then
     echo "A target backup of file type .$FILE_EXTENSION could not be found in directory $BACKUP_SOURCE_DIR. Exiting."
     return 0
 else
-    echo "Target backup found: $BACKUP_SOURCE_DIR/$TARGET"
+    for TARGET in $(ls -Art ${BACKUP_SOURCE_DIR} | grep ".${FILE_EXTENSION}$" | tail -n $NUM_MOST_RECENT_FILES)
+    do
+        echo "Target backup found: $BACKUP_SOURCE_DIR/$TARGET"
+        echo "Uploading backup to S3..."
+        aws s3 cp $BACKUP_SOURCE_DIR/$TARGET $S3_BUCKET_URL
+
+        if [[ $? = "0" ]]; then
+            echo "Upload successful"
+
+            UPLOAD_STATES="$UPLOAD_STATES$TARGET\\t SUCCESS\\n"
+        else
+            echo "Upload unsuccessful, code $?"
+            UPLOAD_STATES="$UPLOAD_STATES$TARGET\\t UNSUCCESSFUL\\n"
+        fi
+    done
 fi
 
-echo "Uploading backup to S3..."
-aws s3 cp $BACKUP_SOURCE_DIR/$TARGET $S3_BUCKET_URL
 
-if [[ $? = "0" ]]; then
-    echo "Upload successful"
+UPLOAD_STATES=$UPLOAD_STATES'```'
 
-    if [[ -n ${WEBHOOK} ]];
-    then
-        echo "Pinging $WEBHOOK"
-        curl -i -s -m 10 --retry 5 -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"S3 backup successful.\"}" $WEBHOOK
-    fi
-else
-    echo "Upload unsuccessful, code $?"
+echo $UPLOAD_STATES
 
-    if [[ -n ${WEBHOOK} ]];
-    then
-        echo "Pinging $WEBHOOK"
-        curl -i -s -m 10 --retry 5 -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"S3 backup unsuccessful. Error $?.\"}" $WEBHOOK
-    fi
-
-    return 0
+if [[ -n ${WEBHOOK} ]];
+then
+    echo "Pinging $WEBHOOK"
+    echo "{\"content\": \"S3 backup initiated:$UPLOAD_STATES\"}"
+    curl -i -s -m 10 --retry 5 -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"S3 backup initiated:$UPLOAD_STATES\"}" $WEBHOOK
 fi
 
 
