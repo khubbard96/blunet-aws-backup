46946bfd69e77a69bba7b4b0e51d98ea412a143e
unknown <kevlar2020@gmail.com>
2023-03-08 12:43:52 -0600

    updating functionality for passing aws auth as secrets

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+#read in the aws access key information
+if [[ -n ${AWS_ACCESS_KEY_ID_FILE} ]];
+then
+    export AWS_ACCESS_KEY_ID=$(cat $AWS_ACCESS_KEY_ID_FILE)
+fi
+
+if [[ -n ${AWS_SECRET_ACCESS_KEY_FILE} ]];
+then
+    export AWS_SECRET_ACCESS_KEY=$(cat $AWS_SECRET_ACCESS_KEY_FILE)
+fi
+
+#cron job
 echo "creating crontab"
 echo -e "$CRON_SCHEDULE /backup-and-cleanup.sh\n" > /etc/crontabs/root
 echo "starting crond"
